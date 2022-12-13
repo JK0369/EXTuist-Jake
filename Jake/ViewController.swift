34402edeab510f48519f4d5d73b92ab80c616dcd
jake <palatable7@naver.com>
2022-12-14 00:28:13 +0900

    Implement

---
+++ Jake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jake
+//
+//  Created by 김종권 on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+// MARK: Constants
+let projectName = "Jake"
+let organizationName = "jake"
+let bundleID = "com.jake.Jake"
+let targetVersion = "14.0"
+
+// MARK: Struct
+let project = Project(
+  name: projectName,
+  organizationName: organizationName,
+  packages: [],
+  settings: nil,
+  targets: [
+    Target(name: projectName,
+           platform: .iOS,
+           product: .staticFramework,
+           bundleId: bundleID,
+           deploymentTarget: .iOS(targetVersion: targetVersion, devices: [.iphone]),
+           infoPlist: .default,
+           sources: ["\(projectName)/**"],
+           resources: [],
+           dependencies: [] // tuist generate할 경우 pod install이 자동으로 실행
+          )
+  ],
+  schemes: [
+    Scheme(name: "\(projectName)-Debug"),
+    Scheme(name: "\(projectName)-Release")
+  ],
+  fileHeaderTemplate: nil,
+  additionalFiles: [],
+  resourceSynthesizers: []
+)
